f67771162b581167aaa313a464bdbed632f91e78
gingercook <vahitaras@gmail.com>
2021-09-18 23:11:58 -0400

    Name surname added!

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Aras';
+const fName = 'Aras';
+const lName = 'Sen';
 const randNum = Math.floor(Math.random()*6+1);
 
 // 2 Parameters, what to show, where to show
+// Curly braces will accept one line javascript code.
+// Simply it takes expressions but not statements.
 ReactDOM.render(
   <div>
     <h1>My Favorite Foods</h1>
-    <p>My name is {name}</p>
-    <p>My random number is {randNum}</p>
+    <p>My name is {`${fName} ${lName}`}.</p>
+    <p>My random number is {randNum}.</p>
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
