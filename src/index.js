ef0ab6120188eb342b26d6776fbfec606fa7a296
gingercook <vahitaras@gmail.com>
2021-09-18 22:56:10 -0400

    javascript embedded inside HTML which is embedded inside Javascript!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 // This React module will focus on ES6
 
-// const React = require("react");
-import React from "react";
+// const React = require('react');
+// const ReactDOM = require('react-dom');
 
-// const ReactDOM = require("react-dom");
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'Aras';
+const randNum = Math.floor(Math.random()*6+1);
 
 // 2 Parameters, what to show, where to show
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph!</p>
-  </div>,
-  document.getElementById("root")
+    <h1>My Favorite Foods</h1>
+    <p>My name is {name}</p>
+    <p>My random number is {randNum}</p>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>, 
+  document.getElementById('root')
 );
