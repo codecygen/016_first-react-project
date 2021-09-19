67f15454f9663724686d88b5c7b3737dd1a6038b
gingercook <vahitaras@gmail.com>
2021-09-18 23:22:20 -0400

    Dynamic date added!

--- src/index.js
+++ src/index.js
@@ -10,19 +10,36 @@ const fName = 'Aras';
 const lName = 'Sen';
 const randNum = Math.floor(Math.random()*6+1);
 
+const getDate = () => {
+  const today = new Date;
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  return today.toLocaleDateString('en-US', options);
+}
+
+const date = getDate();
+
+
 // 2 Parameters, what to show, where to show
 // Curly braces will accept one line javascript code.
 // Simply it takes expressions but not statements.
 ReactDOM.render(
   <div>
     <h1>My Favorite Foods</h1>
-    <p>My name is {`${fName} ${lName}`}.</p>
-    <p>My random number is {randNum}.</p>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <li>Butter Chicken</li>
+      <li>Sushi</li>
+      <li>Pizza</li>
     </ul>
+    <p>This app is created by {`${fName} ${lName}`}.</p>
+    <p>My random number is {randNum}.</p>
+    <p>Today is {date}.</p>
   </div>, 
   document.getElementById('root')
 );
