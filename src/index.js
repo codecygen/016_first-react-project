1121dcd7869d35531d902408eb86d28e552de96c
gingercook <vahitaras@gmail.com>
2021-09-18 22:14:03 -0400

    First commit!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This React module will focus on ES6
 
+// const React = require("react");
+import React from "react";
+
+// const ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+
+// 2 Parameters, what to show, where to show
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph!</p>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
